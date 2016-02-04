4ff3e7ef8a34bbcecb9474c686bac9da63338da2
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-02-04 07:01:25 +0100

    Code restructuration

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from distutils.core import setup
-from distutils.extension import Extension
+# -----------------------------------------------------------------------------
+# Copyright (c) 2016, Nicolas P. Rougier
+# Distributed under the (new) BSD License.
+# -----------------------------------------------------------------------------
+# from distutils.core import setup
+# from distutils.extension import Extension
+# from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
+
 extensions = [
-    Extension('cdana', ['cdana.pyx'], include_dirs = [np.get_include()]),
+    Extension('cdana/cdana', ['cdana/cdana.pyx'], include_dirs = [np.get_include()]),
 ]
-setup( ext_modules = cythonize(extensions) )
+setup(
+    name="cdana",
+    version="0.1",
+    maintainer= "Nicolas P. Rougier",
+    maintainer_email="Nicolas.Rougier@inria.fr",
+    install_requires=['numpy', 'cython'],
+    license = "BSD License",
+    packages=['cdana'],
+    ext_modules = cythonize(extensions)
+)
 
 
 
