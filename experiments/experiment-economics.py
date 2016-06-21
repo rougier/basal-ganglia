898facfc9b2f89262af5d94147bd88e1e030b134
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-06-21 13:14:51 +0200

    Added moving average for displayiong performances

--- experiments/experiment-economics.py
+++ experiments/experiment-economics.py
@@ -22,25 +22,29 @@ experiment = Experiment(model = "model-guthrie.json",
                         report = "data/experiment-economics.txt",
                         n_session = 50, n_block = 1, seed = None)
 records = experiment.run(session, "Progress")
-
 records = np.squeeze(records)
+
+# Since the task is two steps, we take only one trial out of 2 (when reward is
+# actually received, hence starting a trial 1)
 records = records[:,1::2]
 n_trial = len(experiment.task)/2
 
 
 # -----------------------------------------------------------------------------
-#P_mean = np.mean(records["reward"]*2, axis=0)
-#P_std = np.std(records["reward"]*2, axis=0)
-P_mean = np.mean(records["reward"], axis=0)
-P_std = np.std(records["reward"], axis=0)
 
-RT_mean = np.mean(records["RT"]*1000, axis=0)
-RT_std = np.std(records["RT"]*1000, axis=0)
+# Moving average over n trials
+n = 10
+R = records["reward"]
+R = np.cumsum(R, axis=-1, dtype=float)
+R[:,n:] = R[:,n:] - R[:,:-n]
+R = R[:,n-1:]/n
+n_trial = R.shape[1]
 
+P_mean = np.mean(R, axis=0)
+P_std = np.std(R, axis=0)
 
 
 plt.figure(figsize=(16,6), facecolor="w")
-
 
 ax = plt.subplot(111)
 ax.patch.set_facecolor("w")
@@ -51,25 +55,28 @@ ax.yaxis.set_tick_params(direction="in")
 ax.xaxis.set_ticks_position('bottom')
 ax.xaxis.set_tick_params(direction="in")
 
-X = 1+np.arange(n_trial)
+X = n+np.arange(n_trial)
 
 plt.plot(X, P_mean, c='b', lw=2)
 plt.plot(X, P_mean + P_std, c='b', lw=.5)
 plt.plot(X, P_mean - P_std, c='b', lw=.5)
 plt.fill_between(X, P_mean + P_std, P_mean - P_std, color='b', alpha=.1)
 
-plt.text(n_trial+1, P_mean[-1], "%.2f" % P_mean[-1],
+plt.text(n_trial+n, P_mean[-1], "%.2f" % P_mean[-1],
          ha="left", va="center", color="b")
 
 plt.ylabel("Performance\n", fontsize=16)
-plt.xlim(1,n_trial)
-plt.ylim(0,1.25)
+plt.xlim(0,n+n_trial)
+plt.ylim(0,1.1)
 
-plt.yticks([ 0.0,   0.2,   0.4,  0.6, 0.8,   1.0])
-plt.text(0, P_mean[0], "%.2f" % P_mean[0],
-         ha="right", va="center", color="b")
+ax.axvspan(0,n, color='.975')
+ax.axhline(1.0, color='.5', lw=.5, ls = "--")
+ax.axhline(0.5, color='.5', lw=.5, ls = "--")
 
+plt.yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
+#plt.text(-1, P_mean[0], "%.2f" % P_mean[0],
+#         ha="right", va="center", color="b")
 
-plt.savefig("data/experiment-guthrie.pdf")
+plt.savefig("data/experiment-economics.pdf")
 plt.show()
 
